db23e21faee7c232adffd8213ea599f2444bb37c
abdo-coder <a.h2020.2007@gmail.com>
2020-04-13 19:24:38 +0200

    adding server code

---
+++ client/precache-manifest.71b153e6ce1d6703ccf4f315355b68b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d842a892058f7b92ae65c6af3c21ba54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb7d6af3effa37513039",
+    "url": "/static/css/2.a2c18f5e.chunk.css"
+  },
+  {
+    "revision": "7673f1ccbd41937c63c2",
+    "url": "/static/css/main.8f9da5ad.chunk.css"
+  },
+  {
+    "revision": "eb7d6af3effa37513039",
+    "url": "/static/js/2.a2dadd53.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a2dadd53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7673f1ccbd41937c63c2",
+    "url": "/static/js/main.dea7faf9.chunk.js"
+  },
+  {
+    "revision": "429a3bfed2c3b7a8c60a",
+    "url": "/static/js/runtime-main.e19a20ae.js"
+  }
+]);
